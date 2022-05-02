2c046856abb7a8bf12ba09a7bdba606a3e3981c6
Mārtiņš Mednis <mrt@mednis.info>
2022-05-02 17:15:40 +0300

    files for packaging

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mathjson-solver",
+    version="0.0.1",
+    author="Martins Mednis",
+    author_email="mrt@mednis.info",
+    description="Utilities for MathJSON evaluation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LongenesisLtd/mathjson-solver",
+    project_urls={
+        "Bug Tracker": "https://github.com/LongenesisLtd/mathjson-solver/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
